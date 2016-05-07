900c646daf8268c976cfb8439fff129215ab5a13
adrienplg <adrienplg@gmail.com>
2016-05-07 16:04:45 -0700

    added angular and a directive for bootsrap navbar example

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 (function (){
+
 	// Main module
-	var app = angular.module('quidlingua', []);
+	var app = angular.module('quidLingua', []);
 
 	// Index page controller
 	app.controller('MainPageController', function(){
 		var frenchMainPage = { name: 'french_main', url: 'fr/index.html'}
 	});
+
+  app.directive('bootstrapNavbar', function(){
+    return{
+      restrict: 'E',
+      templateUrl: 'bs-navbar.html'
+    };
+  });
+
 })();
